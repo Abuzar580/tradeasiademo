15f5308aa86f3f7b6231a383039c38dc0917bd1b
Abuzar580 <abuzar.iftikhar@nestosh.com>
2026-03-07 00:26:54 +0500

    Global Supply Section
     - Implemented the Global Supply section component and integrated it into the Layout One homepage structure.
     - Integrated the section into HomeLayoutOne, ensuring the homepage structure remains aligned with the layout-based architecture introduced earlier.

--- src/layouts/layout-one/HomeLayoutOne.tsx
+++ src/layouts/layout-one/HomeLayoutOne.tsx
@@ -1,9 +1,11 @@
 import { AppDownloadSection } from "@/components/sections/AppDownloadSection";
 import { FeedbackSection } from "@/components/sections/FeedbackSection";
+import { GlobalSupplySection } from "@/components/sections/GlobalSupplySection";
 
 export function HomeLayoutOne() {
   return (
     <>
+      <GlobalSupplySection/>
       <AppDownloadSection />
       <FeedbackSection headingPrefix="Give us your" headingHighlight="Feedback" subtitle="Let us know how we can serve you better" buttonText="SUBMIT YOUR FEEDBACK" buttonHref="#" backgroundImage="/feedback.png"/>
     </>
