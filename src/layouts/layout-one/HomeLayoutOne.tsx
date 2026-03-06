a410c6ce5f9374eed6758d14fb49de1eb2ccf28c
Abuzar580 <abuzar.iftikhar@nestosh.com>
2026-03-06 17:44:11 +0500

    Feedback Section [Homepage]

--- src/layouts/layout-one/HomeLayoutOne.tsx
+++ src/layouts/layout-one/HomeLayoutOne.tsx
@@ -1,9 +1,11 @@
 import { AppDownloadSection } from "@/components/sections/AppDownloadSection";
+import { FeedbackSection } from "@/components/sections/FeedbackSection";
 
 export function HomeLayoutOne() {
   return (
     <>
       <AppDownloadSection />
+      <FeedbackSection headingPrefix="Give us your" headingHighlight="Feedback" subtitle="Let us know how we can serve you better" buttonText="SUBMIT YOUR FEEDBACK" buttonHref="#" backgroundImage="/feedback.png"/>
     </>
   );
 }
