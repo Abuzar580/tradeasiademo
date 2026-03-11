8dfed8e8335907fd439bbd89e4a0e233d114ebc4
Talha-Hamayoun <talha.humayun@nestosh.com>
2026-03-12 01:04:46 +0500

    update card fixes

--- src/components/sections/OurTopProductsSection/OurTopProductsSection.tsx
+++ src/components/sections/OurTopProductsSection/OurTopProductsSection.tsx
@@ -9,7 +9,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { CheckIcon, FileIcon, StackIcon } from "@/icons";
+import { ArrowIcon, CheckIcon, FileIcon, StackIcon } from "@/icons";
 
 type ProductsSectionProps = {
   title: string;
@@ -84,7 +84,7 @@ export function OurTopProductsSection({
   subtitle
 }: ProductsSectionProps) {
   return (
-    <section className="relative flex justify-end  overflow-hidden bg-[var(--brand-primary)] md:bg-[var(--brand-black)] py-15 text-center md:py-16 lg:py-[120px]">
+    <section className="relative flex justify-center overflow-hidden bg-[var(--brand-primary)] md:bg-[var(--brand-black)] py-15 text-center md:py-16 lg:py-[120px]">
       <Image
         src="/ArtWorkIcon.svg"
         alt=""
@@ -106,9 +106,9 @@ export function OurTopProductsSection({
       />
 
       {/* Products Slider */}
-      <div className="w-full flex flex-col items-center relative justify-center md:justify-end md:flex-row max-w-[1140px] mx-auto md:mx-[100px] z-10 md:bg-[var(--brand-neutral-surface-2)] md:py-[76px] md:px-[34px] md:rounded-2xl">
+      <div className="w-full flex flex-col items-center md:right-[-104px] relative justify-center md:justify-end md:flex-row max-w-[1140px] z-10 md:bg-[var(--brand-neutral-surface-2)] md:py-[76px] md:px-[34px] md:rounded-2xl">
         {/* Desktop & Mobile Title */}
-        <div className="flex flex-col w-full md:absolute md:left-[-190px] md:w-[385px] md:h-[478px] items-center justify-center gap-6 bg-[rgba(var(--brand-primary-rgb),0.9)] p-4 md:p-6 rounded-none md:rounded-3xl py-8 md:py-0">
+        <div className="flex flex-col w-full md:absolute md:left-[-206px] md:w-[385px] md:h-[478px] items-center justify-center gap-6 bg-[rgba(var(--brand-primary-rgb),0.9)] p-4 md:p-6 rounded-none md:rounded-3xl py-8 md:py-0">
           <div className="flex flex-col gap-[30px] text-center">
             <h1
               className="
@@ -138,18 +138,18 @@ export function OurTopProductsSection({
           <div className="hidden md:flex items-center justify-between w-full md:max-w-[300px] max-w-[320px]">
             <Button
               variant="outlined"
-              className="custom-swiper-prev inline-flex items-center justify-center shadow-none! p-3!"
+              className="custom-swiper-prev inline-flex items-center justify-center rounded-full disabled:cursor-not-allowed disabled:opacity-[0.3]"
             >
-              <StackIcon className="w-6 h-6" />
+              <ArrowIcon className="w-4 h-4 rotate-180" />
             </Button>
 
             <div className="hidden md:flex custom-swiper-pagination items-center justify-center gap-2 [&_.swiper-pagination-bullet-active]:!w-[32px] [&_.swiper-pagination-bullet-active]:!rounded-[20px] [&_.swiper-pagination-bullet-active]:!bg-[var(--brand-white)] [&_.swiper-pagination-bullet]:!bg-[var(--brand-white)]" />
 
             <Button
               variant="outlined"
-              className="custom-swiper-next inline-flex items-center justify-center shadow-none! p-3!"
+              className="custom-swiper-next inline-flex items-center justify-center rounded-full disabled:cursor-not-allowed disabled:opacity-[0.3]"
             >
-              <StackIcon className="w-6 h-6 rotate-180" />
+              <ArrowIcon className="w-4 h-4" />
             </Button>
           </div>
         </div>
@@ -193,56 +193,57 @@ export function OurTopProductsSection({
               spaceBetween: 30,
             },
           }}
-          className="m-0! w-full md:max-w-[880px] p-[50px]! md:p-0! py-0! md:py-0!"
+          className="m-0! w-full md:max-w-[900px] p-[50px]! md:p-0! py-0! md:py-0!"
         >
           {PRODUCTS.map((product) => (
             <SwiperSlide key={product.id} className="!h-auto">
               <div className="rounded-xl overflow-hidden h-full flex flex-col">
-                <div className="relative w-full h-48 md:h-[280px] bg-[var(--brand-neutral-surface-2)]">
+                <div className="relative w-full h-[533px] bg-[var(--brand-neutral-surface-2)]">
                   <Image
                     src={product.image}
                     alt={product.name}
-                    fill
-                    className="object-cover"
+                    width={280}
+                    height={280}
+                    className="w-full h-[280px] object-cover"
                   />
-                </div>
+                  <div className="absolute w-full bottom-0 rounded-t-2xl bg-[var(--brand-primary-deep)] z-50! p-[28px] flex flex-col gap-[20px] flex-1">
+                    {/* Product Name */}
+                    <h3 className="text-start body-large text-[var(--brand-white)] line-clamp-2">
+                      {product.name}
+                    </h3>
 
-                <div className="bg-[var(--brand-primary-deep)] p-4 md:p-6 flex flex-col gap-[15px] flex-1">
-                  {/* Product Name */}
-                  <h3 className="text-start body-large text-[var(--brand-white)] line-clamp-2">
-                    {product.name}
-                  </h3>
-
-                  {/* Product Details with Icons */}
-                  <div className="space-y-4 flex-1">
-                    <div className="flex items-center gap-3">
-                      <div className="bg-[var(--brand-white)] rounded-lg flex-shrink-0 w-8 h-8 flex items-center justify-center">
-                        <product.checkIcon />
+                    {/* Product Details with Icons */}
+                    <div className="space-y-4 flex-1">
+                      <div className="flex items-center gap-3">
+                        <div className="bg-[var(--brand-white)] rounded-lg flex-shrink-0 w-8 h-8 flex items-center justify-center">
+                          <product.checkIcon className="w-4 h-4 text-[var(--brand-primary)]" />
+                        </div>
+                        <p className="body-caption text-[var(--brand-white)]">
+                          <span className="body-caption">CAS NUMBER:</span> {product.casNumber}
+                        </p>
                       </div>
-                      <p className="body-caption text-[var(--brand-white)]">
-                        <span className="body-caption">CAS NUMBER:</span> {product.casNumber}
-                      </p>
+
+                      <div className="flex items-center gap-3">
+                        <div className="bg-[var(--brand-white)] rounded-lg flex-shrink-0 w-8 h-8 flex items-center justify-center">
+                          <product.fileIcon className="w-4 h-4 text-[var(--brand-primary)]" />
+                        </div>
+                        <p className="body-caption text-[var(--brand-white)]">
+                          <span className="body-caption">HS CODE:</span> {product.hsCode}
+                        </p>
+                      </div>
                     </div>
 
-                    <div className="flex items-center gap-3">
-                      <div className="bg-[var(--brand-white)] rounded-lg flex-shrink-0 w-8 h-8 flex items-center justify-center">
-                        <product.fileIcon />
-                      </div>
-                      <p className="body-caption text-[var(--brand-white)]">
-                        <span className="body-caption">HS CODE:</span> {product.hsCode}
-                      </p>
-                    </div>
+                    {/* Inquire Button */}
+                    <Button
+                      variant="outlined"
+                      href="#"
+                      className="w-full mt-auto"
+                    >
+                      INQUIRE NOW
+                    </Button>
                   </div>
-
-                  {/* Inquire Button */}
-                  <Button
-                    variant="outlined"
-                    href="#"
-                    className="w-full mt-auto"
-                  >
-                    INQUIRE NOW
-                  </Button>
                 </div>
+
               </div>
             </SwiperSlide>
           ))}
@@ -251,18 +252,18 @@ export function OurTopProductsSection({
         <div className="flex md:hidden mt-10 items-center justify-between w-full md:max-w-[300px] max-w-[320px]">
           <Button
             variant="outlined"
-            className="custom-swiper-prev inline-flex items-center justify-center shadow-none!"
+            className="custom-swiper-prev inline-flex items-center justify-center rounded-full disabled:cursor-not-allowed disabled:opacity-[0.3]"
           >
-            <StackIcon className="w-6 h-6" />
+            <ArrowIcon className="w-4 h-4 rotate-180" />
           </Button>
 
           {/* <div className="hidden md:flex custom-swiper-pagination items-center justify-center gap-2 [&_.swiper-pagination-bullet-active]:!w-[32px] [&_.swiper-pagination-bullet-active]:!rounded-[20px] [&_.swiper-pagination-bullet-active]:!bg-[var(--brand-white)] [&_.swiper-pagination-bullet]:!bg-[var(--brand-white)]" /> */}
 
           <Button
             variant="outlined"
-            className="custom-swiper-next inline-flex items-center justify-center shadow-none!"
+            className="custom-swiper-next inline-flex items-center justify-center rounded-full disabled:cursor-not-allowed disabled:opacity-[0.3]"
           >
-            <StackIcon className="w-6 h-6 rotate-180" />
+            <ArrowIcon className="w-4 h-4" />
           </Button>
         </div>
       </div>
