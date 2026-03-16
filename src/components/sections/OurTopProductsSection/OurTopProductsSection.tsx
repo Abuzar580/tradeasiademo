b6fb34b7d07ee80f4fffdd6c917ff084758e823b
Talha-Hamayoun <talha.humayun@nestosh.com>
2026-03-16 23:27:17 +0500

    update max width

--- src/components/sections/OurTopProductsSection/OurTopProductsSection.tsx
+++ src/components/sections/OurTopProductsSection/OurTopProductsSection.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui";
 import Image from "next/image";
 
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -13,15 +13,20 @@ import "swiper/css/pagination";
 
 import { ArrowIcon, CheckIcon, FileIcon, StackIcon } from "@/icons";
 
+type ProductsCard = {
+  id: number;
+  name: string;
+  casNumber: string;
+  hsCode: string;
+  image: string;
+  CheckIcon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  FileIcon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+};
+
 type ProductsSectionProps = {
   title: string;
   subtitle: string;
-};
-
-export type FeatureCard = {
-  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-  value: string;
-  label: string;
+  products?: ProductsCard[];
 };
 
 const PRODUCTS = [
@@ -86,7 +91,7 @@ export function OurTopProductsSection({
   subtitle
 }: ProductsSectionProps) {
   return (
-    <section className="relative flex justify-center overflow-hidden bg-[var(--brand-primary)] md:bg-[var(--brand-black)] py-15 pb-[120px] text-center md:py-16 lg:py-[120px]">
+    <section className="relative flex justify-center overflow-hidden bg-[var(--brand-primary)] md:bg-[var(--brand-black)] py-[40px] md:py-[80px] pb-[120px] text-center md:py-16 lg:py-[120px]">
       <Image
         src="/ArtWorkIcon.svg"
         alt=""
@@ -108,18 +113,20 @@ export function OurTopProductsSection({
       />
 
       {/* Products Slider */}
-      <div className="w-full flex flex-col items-center md:right-[-104px] relative justify-center md:justify-end md:flex-row max-w-[1140px] z-10 md:bg-[var(--brand-neutral-surface-2)] md:py-[76px] md:px-[34px] md:rounded-2xl">
+      <div className="w-full md:px-[40px] flex flex-col items-center relative justify-center md:justify-end md:flex-row z-10 md:gap-[30px]">
+        <div className=" absolute w-[calc(100%-240px)] h-full md:bg-[var(--brand-neutral-surface-2)] md:rounded-2xl"> </div>
         {/* Desktop & Mobile Title */}
-        <div className="flex flex-col w-full md:absolute md:left-[-206px] md:w-[385px] md:h-[478px] items-center justify-center gap-6 bg-[rgba(var(--brand-primary-rgb),0.9)] p-4 md:p-6 rounded-none md:rounded-3xl py-8 md:py-0">
-          <div className="flex flex-col gap-[30px] text-center">
+        <div className="flex flex-col w-full md:w-[545px] z-50 items-center justify-center bg-[rgba(var(--brand-primary-rgb),0.9)] md:p-6 md:py-[72px] rounded-none md:rounded-3xl">
+          <div className="flex flex-col gap-[30px] text-center md:px-0 px-[52px] pb-[50px] md:pb-[40px]">
             <h1
               className="
               text-[length:var(--heading-3-size)]
               leading-[var(--heading-3-line-height)]
-              font-[var(--font-weight-bold)]
+              font-[var(--font-weight-semibold)]
               md:text-[length:var(--heading-2-size)]
               md:leading-[var(--heading-2-line-height)]
               text-[var(--brand-white)]
+              md:px-0 px-[50px]
             "
             >
               {title}
@@ -128,8 +135,9 @@ export function OurTopProductsSection({
             {subtitle && (
               <p
                 className="
-                leading-[var(--body-large-line-height)]
-                font-[var(--font-weight-semibold)]
+                body-large
+                font-[var(--font-weight-medium)]!
+                leading-[var(--body-large-line-height)]!
                 text-[var(--brand-white)]
               "
               >
@@ -142,23 +150,22 @@ export function OurTopProductsSection({
             <div className="flex justify-start">
               <Button
                 variant="outlined"
-                className="events-swiper-prev inline-flex items-center justify-center shadow-none! p-3! [&.swiper-button-disabled]:opacity-30 h-[50px] w-[50px]"
+                className="products-swiper-prev inline-flex items-center justify-center shadow-none! p-3! [&.swiper-button-disabled]:opacity-30 h-[50px] w-[50px]"
               >
                 <ArrowIcon className="w-4 h-3 rotate-180" />
               </Button>
             </div>
-            <div className="flex events-swiper-pagination items-center justify-center gap-2 [&_.swiper-pagination-bullet-active]:!w-[40px] [&_.swiper-pagination-bullet-active]:!rounded-[20px] [&_.swiper-pagination-bullet-active]:!bg-[var(--brand-white)] [&_.swiper-pagination-bullet]:!bg-[var(--brand-white)]" />
+            <div className="flex products-swiper-pagination items-center justify-center gap-2 [&_.swiper-pagination-bullet-active]:!w-[40px] [&_.swiper-pagination-bullet-active]:!rounded-[20px] [&_.swiper-pagination-bullet-active]:!bg-[var(--brand-white)] [&_.swiper-pagination-bullet]:!bg-[var(--brand-white)]" />
             <div className="flex justify-end">
               <Button
                 variant="outlined"
-                className="events-swiper-next inline-flex items-center justify-center shadow-none! p-3! [&.swiper-button-disabled]:opacity-30 h-[50px] w-[50px]"
+                className="products-swiper-next inline-flex items-center justify-center shadow-none! p-3! [&.swiper-button-disabled]:opacity-30 h-[50px] w-[50px]"
               >
                 <ArrowIcon className="w-4 h-3" />
               </Button>
             </div>
           </div>
         </div>
-
         <Swiper
           modules={[Navigation, Pagination]}
           spaceBetween={110}
@@ -168,12 +175,12 @@ export function OurTopProductsSection({
           allowTouchMove={false}
           loop={false}
           navigation={{
-            prevEl: ".events-swiper-prev",
-            nextEl: ".events-swiper-next",
+            prevEl: ".products-swiper-prev",
+            nextEl: ".products-swiper-next",
           }}
           pagination={{
             clickable: true,
-            el: ".events-swiper-pagination",
+            el: ".products-swiper-pagination",
             renderBullet: (index, className) =>
               `<span class="${className} h-[6px] w-[6px] rounded-[10px] bg-[var(--brand-white)] cursor-pointer transition-all duration-300"></span>`,
           }}
@@ -195,18 +202,18 @@ export function OurTopProductsSection({
               spaceBetween: 30,
             },
           }}
-          className="industries-swiper m-0! w-full md:max-w-[900px] p-[50px]! md:p-0! py-0! md:py-0!"
+          className="industries-swiper m-0! w-full p-[50px]! md:p-0! py-0! md:pr-[32px]! md:py-[76px]!"
         >
           {PRODUCTS.map((product) => (
             <SwiperSlide key={product.id} className="!h-auto">
-              <div className="rounded-xl overflow-hidden h-full flex flex-col border-3 border-transparent hover:border-[var(--brand-primary)] hover:shadow-[5px_5px_19px_var(--brand-primary-hover)] transition-all duration-300 ease-in-out">
+              <div className="rounded-xl overflow-hidden h-full flex flex-col border-3 border-transparent hover:border-[var(--brand-primary)] hover:shadow-[1px_1px_6px_var(--brand-primary-hover)] hover:border-[5px_3px_10px_var(--brand-primary-hover)] transition-all duration-800 ease-in-out">
                 <div className="relative w-full h-[533px] bg-[var(--brand-neutral-surface-2)]">
                   <Image
                     src={product.image}
                     alt={product.name}
                     width={280}
                     height={280}
-                    className="w-full h-[280px] object-cover"
+                    className="w-full h-[286px] object-cover"
                   />
                   <div className="absolute w-full bottom-0 rounded-t-2xl bg-[var(--brand-primary-deep)] z-50! p-[26px] flex flex-col gap-[20px] flex-1">
                     {/* Product Name */}
@@ -249,6 +256,7 @@ export function OurTopProductsSection({
             </SwiperSlide>
           ))}
         </Swiper>
+
       </div>
     </section>
   );
